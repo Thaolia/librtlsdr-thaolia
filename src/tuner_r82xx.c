94289a6979c8bf79d38f835debd6ad0a1c517aed
james <james@brunhild.tmp.li>
2017-06-20 12:07:38 +0200

    VGA Gain patch from http://www.pabr.org/radio/leandvb-satmodem/leandvb-satmodem.en.html

--- src/tuner_r82xx.c
+++ src/tuner_r82xx.c
@@ -1199,7 +1199,9 @@ int r82xx_set_gain(struct r82xx_priv *priv, int set_manual_gain, int gain,
 	  return rc;
 
 	/* set fixed VGA gain for now (16.3 dB) */
-	rc = r82xx_write_reg_mask(priv, 0x0c, 0x08, 0x9f);
+	//rc = r82xx_write_reg_mask(priv, 0x0c, 0x08, 0x9f);
+	/* set max VGA gain */
+	rc = r82xx_write_reg_mask(priv, 0x0c, 0x0f, 0x9f);
 	if (rc < 0)
 	  return rc;
 
@@ -1236,7 +1238,9 @@ int r82xx_set_gain(struct r82xx_priv *priv, int set_manual_gain, int gain,
 	  return rc;
 
 	/* set fixed VGA gain for now (26.5 dB) */
-	rc = r82xx_write_reg_mask(priv, 0x0c, 0x0b, 0x9f);
+	//rc = r82xx_write_reg_mask(priv, 0x0c, 0x0b, 0x9f);
+	/* set max VGA gain */
+	rc = r82xx_write_reg_mask(priv, 0x0c, 0x0f, 0x9f);
 	if (rc < 0)
 	  return rc;
   }
